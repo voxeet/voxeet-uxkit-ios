e8f5440eb1b492526b22cd13d15278f1b5ef93ec
JiangRi <78546492+jiangri927@users.noreply.github.com>
2022-02-04 18:18:39 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,14 +3,39 @@ import PackageDescription
 
 let package = Package(
     name: "VoxeetUXKit",
+    defaultLocalization: "en",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v12)
     ],
     products: [
         .library(name: "VoxeetUXKit", targets: ["VoxeetUXKit"])
     ],
-    targets: [
-        .target(name: "VoxeetUXKit", dependencies: []),
-        .testTarget(name: "VoxeetUXKitTests", dependencies: ["VoxeetUXKit"])
+    dependencies: [
+      .package(
+        name: "VoxeetSDK",
+        url: "https://github.com/voxeet/voxeet-sdk-ios.git",
+        from: "3.3.3"
+      ),
+      .package(
+        name: "Kingfisher",
+        url: "https://github.com/onevcat/Kingfisher.git",
+        from: "7.1.0"
+      )
+    ],
+    targets:
+        [
+        .target(
+            name: "VoxeetUXKit",
+            dependencies: [
+                Target.Dependency.byName(name: "VoxeetSDK"),
+                Target.Dependency.byName(name: "Kingfisher")
+            ],
+            path: "VoxeetUXKit",
+            exclude: ["Other/Info.plist"],
+            resources: [
+                .process("OtherInfo.plist"),
+                .process("Assets/Images"),
+                .process("Assets/Sounds")
+            ])
     ]
 )
